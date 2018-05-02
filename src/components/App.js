d968c80ad997553b5cf4b13209614d6c2f5c568a
Joseph Gordy <jgordy2424@gmail.com>
2018-05-02 17:59:42 -0400

    created the card component and added some organization to the file structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card from './Card.js';
+import data from '../sampleData/UIE-InterviewProject.json';
+import '../styles/App.css';
 
 class App extends Component {
+
   render() {
+    console.log(data);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Card />
       </div>
     );
   }
-}
+};
 
 export default App;
