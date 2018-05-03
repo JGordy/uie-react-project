c90ce83a598de8661160a1f2f611d2dd0d214a90
Joseph Gordy <jgordy2424@gmail.com>
2018-05-02 23:58:12 -0400

    Tweaked the filter options to be created dynamically instead of hardcoded

--- src/components/filter/index.js
+++ src/components/filter/index.js
@@ -3,29 +3,27 @@ import './filter.css';
 
 export default class Filter extends Component {
 
+  createRadios = () => {
+    let options = ['Heading', 'Subheading', 'Price'];
+
+    return options.map((option, index) => {
+      return (
+        <span key={index} style={{margin: '1rem'}}>
+          <label style={{marginLeft: '0.5rem'}}>
+            <input type='radio' className="form-check-input" name='sort' value={option} checked={this.props.sortBy === option} onChange={() => this.props.handleState(option)}/>
+            {option}
+          </label>
+        </span>
+      );
+    });
+  }
+
   render() {
-    console.log(this.props);
+
     return(
       <div className="Filter">
-          <span style={{margin: '1rem'}}>
-            <label style={{marginLeft: '0.5rem'}}>
-              <input type='radio' className="form-check-input" name='sort' value="Heading" checked={this.props.sortBy === 'Heading'} onChange={() => this.props.handleState('Heading')}/>
-              Heading
-            </label>
-          </span>
-          <span style={{margin: '1rem'}}>
-            <label style={{marginLeft: '0.5rem'}}>
-              <input type='radio' className="form-check-input" name='sort' value="Subheading" checked={this.props.sortBy === 'Subheading'} onChange={() => this.props.handleState('Subheading')}/>
-              Subheading
-            </label>
-          </span>
-          <span style={{margin: '1rem'}}>
-              <label style={{marginLeft: '0.5rem'}}>
-                <input type='radio' className="form-check-input" name='sort' value="Price" checked={this.props.sortBy === 'Price'} onChange={() => this.props.handleState('Price')}/>
-                Price
-              </label>
-          </span>
+        {this.createRadios()}
       </div>
     );
   }
-}
+};
